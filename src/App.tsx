491b0ae52177c46348555a73b1fb3aee5b2d5f32
onoue9 <filipeonouex@gmail.com>
2022-06-25 19:26:05 -0300

    <feat> Iniciate project :rocket:

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Card } from "./components/card"
 function App() {
   return (
     <main>
-      <h1>Deck Builder</h1>
+      <h1>Yugioh Deck Builder</h1>
       <div className="flex flex-wrap content-center">
         <Card />
       </div>
